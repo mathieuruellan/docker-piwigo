d56f411a0d7817c2ec299b30dc1a38b124785454
Mike Cronce <mike@quadra-tec.net>
2017-11-03 16:30:46 -0400

    entrypoint.sh:  For every file in /config/php/apache2.d/, symlink to that file from /etc/php/7.0/apache2/conf.d/

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,9 +5,15 @@ for d in $(ls /template); do
 
 done
 
+mkdir -pv /config/php/apache2.d
+find /config/php/apache2.d -type f | while read file; do
+	ln -svf "${file}" "/etc/php/7.0/apache2/conf.d/$(basename "${file}")";
+done;
+
 sed -i "s/\/var\/www\/html/\/var\/www/g"  /etc/apache2/sites-enabled/000-default.conf
 
 chown -R www-data:www-data /var/www
 
 source /etc/apache2/envvars
 apache2ctl -D FOREGROUND
+
